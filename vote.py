464e65f2e98dcc91944084a9fca87f6ad524b0d2
Vincent Tetteh <vincentchrisbone@gmail.com>
2019-07-24 14:29:49 +0000

    Use tk's IntVar binding approach

--- vote.py
+++ vote.py
@@ -1,6 +1,10 @@
 from gpiozero import LED,Button
 
+import tkinter as tk
 
+root = tk.Tk()
+app = tk.Frame(root)
+app.pack()
 
 button1 = Button(6)
 button2 = Button(12)
@@ -10,36 +14,38 @@ led1 = LED(18)
 led3 = LED(22)
 led2 = LED(23)
 
-countVote1 = 0
-countVote2 = 0
-countVote3 = 0
-
+countVote1 = tk.IntVar()
+countVote1.set(0)
+countVote2 = tk.IntVar()
+countVote2.set(0)
+countVote3 = tk.IntVar()
+countVote3.set(0)
 
 def addvote1():
-    global countVote1
     led1.on()
     button1.when_released = led1.off
-    countVote1 += 1
-    print (" BENEDICTA final resultS = ", countVote1)
+    countVote1.set(1 + countVote1.get())
+    print ("\tBENEDICTA final resultS = ", countVote1.get(),"\n")
 
 def addvote2():
-    global countVote2
     led2.on()
     button2.when_released = led2.off
-    countVote2 += 1
-    print (" BRIDGET final resultS = ", countVote2)
+    countVote2.set(1 + countVote2.get())    
+    print ("\tBRIDGET final resultS = ", countVote2.get(),"\n")
 
 def addvote3():
-    global countVote3
     led3.on()
     button3.when_released = led3.off
-    countVote3 += 1
-    print ( " ALEX final resultS = ", countVote3) 
+    countVote3.set(1 + countVote3.get())
+    print ( "\tALEX final resultS = ", countVote3.get(),"\n") 
 
 print("ELECTORAL COMMISSION OF GHANA")
-print("HO TECHNICAL SRC ELECTIONS RESULT")
+print("HO TECHNICAL SRC ELECTION RESULT")
 button1.when_pressed = addvote1
 button2.when_pressed = addvote2
 button3.when_pressed = addvote3
 
 
+tkB = tk.Button(app, command=countVote1)
+tkB.pack()
+root.mainloop()
